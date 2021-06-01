8ad53546f3dd782f200ee0e28e0d5076414a7b39
hm5961 <hm5961@naver.com>
2021-06-02 01:41:57 +0900

    end

--- hw12-sorting-hashing.c
+++ hw12-sorting-hashing.c
@@ -180,18 +180,18 @@ int selectionSort(int *a)
 		min = a[i];
 		for(j = i+1; j < MAX_ARRAY_SIZE; j++) // 내부 루프에서 탐색해서 
 		{
-			if (min > a[j]) // 더 작은 값이 있다면 교체 
+			if (min > a[j]) // 더 작은 값이 있다면 임시저장 
 			{
 				min = a[j];
 				minindex = j;
 			}
 		}
-		a[minindex] = a[i]; // 교체된 값으로 변경 
+		a[minindex] = a[i]; // 임시저장 돤 값으로 변경 
 		a[i] = min;
 	}
 
 	printf("----------------------------------------------------------------\n");
-	printArray(a); // 기존 배열 출력 
+	printArray(a); // selection sort된 배열 출력 
 	return 0;
 }
 
@@ -202,22 +202,22 @@ int insertionSort(int *a)
 	printf("Insertion Sort: \n");
 	printf("----------------------------------------------------------------\n");
 
-	printArray(a); 
+	printArray(a); // 기존 배열 출력 
 
-	for(i = 1; i < MAX_ARRAY_SIZE; i++)
+	for(i = 1; i < MAX_ARRAY_SIZE; i++) // 외부르프에서 저장한 값 / 위치로 
 	{
 		t = a[i];
 		j = i;
-		while (a[j-1] > t && j > 0)
+		while (a[j-1] > t && j > 0) // 이전 배열보다 이 후 배열값이 더 크고 j가 0보다 클 때 까지 루프  
 		{
-			a[j] = a[j-1];
-			j--;
+			a[j] = a[j-1]; // i가 1인경우를 제외하고 이 전 루프의 값이 더 크다면 값 변경 
+			j--; // j를 빼가면서 while루프 반복 
 		}
-		a[j] = t;
+		a[j] = t; // 루프를 통해 나온 배열위치에 a[i]값 삽입 
 	}
 
 	printf("----------------------------------------------------------------\n");
-	printArray(a);
+	printArray(a); // Insertion sort된 배열 출력 
 
 	return 0;
 }
@@ -229,13 +229,13 @@ int bubbleSort(int *a)
 	printf("Bubble Sort: \n");
 	printf("----------------------------------------------------------------\n");
 
-	printArray(a);
+	printArray(a); // 기존 배열 출력 
 
-	for(i = 0; i < MAX_ARRAY_SIZE; i++)
+	for(i = 0; i < MAX_ARRAY_SIZE; i++) 
 	{
 		for (j = 0; j < MAX_ARRAY_SIZE; j++)
 		{
-			if (a[j-1] > a[j])
+			if (a[j-1] > a[j]) // 이전 배열보다 이후 배열이 더 작다면 swapnum방식으로 교체 
 			{
 				t = a[j-1];
 				a[j-1] = a[j];
@@ -245,7 +245,7 @@ int bubbleSort(int *a)
 	}
 
 	printf("----------------------------------------------------------------\n");
-	printArray(a);
+	printArray(a); // bubblesort된 배열 출력 
 
 	return 0;
 }
@@ -257,27 +257,27 @@ int shellSort(int *a)
 	printf("Shell Sort: \n");
 	printf("----------------------------------------------------------------\n");
 
-	printArray(a);
+	printArray(a); // 기존 배열 출력 
 
-	for (h = MAX_ARRAY_SIZE/2; h > 0; h /= 2)
+	for (h = MAX_ARRAY_SIZE/2; h > 0; h /= 2) // h를 계속 2로 나누며 0보다 작을 때 까지 루프 
 	{
 		for (i = 0; i < h; i++)
 		{
-			for(j = i + h; j < MAX_ARRAY_SIZE; j += h)
+			for(j = i + h; j < MAX_ARRAY_SIZE; j += h) // j에 h를 더해가며 MAX_ARRAY_SIZE보다 커질 때 까지 루프 
 			{
-				v = a[j];
-				k = j;
-				while (k > h-1 && a[k-h] > v)
+				v = a[j]; // 기존 배열값 임시저장 
+				k = j; // 현재 루프 위치 저장 
+				while (k > h-1 && a[k-h] > v) // k가 h-1보다 작고 a[k-h]가 v보다 작을 때 까지 루프 
 				{
-					a[k] = a[k-h];
-					k -= h;
+					a[k] = a[k-h]; // a[k]에 a[k-h]값 저장 
+					k -= h; // k에서 h빼며 루프 
 				}
-				a[k] = v;
+				a[k] = v; // a[k]에 v값 저장 
 			}
 		}
 	}
 	printf("----------------------------------------------------------------\n");
-	printArray(a);
+	printArray(a); // shellsort된 배열 출력 
 
 	return 0;
 }
@@ -289,25 +289,27 @@ int quickSort(int *a, int n)
 
 	if (n > 1)
 	{
-		v = a[n-1];
+		v = a[n-1]; // 변수 초기값 설정 
 		i = -1;
 		j = n - 1;
 
-		while(1)
+		while(1) 
 		{
-			while(a[++i] < v);
-			while(a[--j] > v);
+			while(a[++i] < v); // a[i]가 v보다 작다면 i증가 반복 
+			while(a[--j] > v); // 
 
-			if (i >= j) break;
-			t = a[i];
+			if (i >= j) break; // i가 j보다 크거나 같을 때 까지 루프 반복 
+			// swapnum 방식으로 교체 
+			t = a[i]; 
 			a[i] = a[j];
 			a[j] = t;
 		}
+		// swapnum 방식으로 교체 
 		t = a[i];
 		a[i] = a[n-1];
 		a[n-1] = t;
 
-		quickSort(a, i);
+		quickSort(a, i); // 재귀를 통해 반복하며 범위 내 정렬을 다시함 
 		quickSort(a+i+1, n-i-1);
 	}
 
@@ -316,22 +318,22 @@ int quickSort(int *a, int n)
 }
 
 int hashCode(int key) {
-   return key % MAX_HASH_TABLE_SIZE;
+   return key % MAX_HASH_TABLE_SIZE; // 인풋받은 key값을 기준으로 0~MAX_HASH_TABLE_SIZE-1의 값으로 리턴 
 }
 
 int hashing(int *a, int **ht)
 {
-	int *hashtable = NULL;
+	int *hashtable = NULL; // 포인터 변수 생성 후 초기화 
 
 	/* hash table이 NULL인 경우 메모리 할당 */
-	if(*ht == NULL) {
-		hashtable = (int*)malloc(sizeof(int) * MAX_ARRAY_SIZE);
+	if(*ht == NULL) { // 인풋받은 ht가 참조하는 값이 NULL이면 
+		hashtable = (int*)malloc(sizeof(int) * MAX_ARRAY_SIZE); // 동적할당 
 		*ht = hashtable;  /* 할당된 메모리의 주소를 복사 --> main에서 배열을 control 할수 있도록 함*/
 	} else {
 		hashtable = *ht;	/* hash table이 NULL이 아닌경우, table 재활용, reset to -1 */
 	}
 
-	for(int i = 0; i < MAX_HASH_TABLE_SIZE; i++)
+	for(int i = 0; i < MAX_HASH_TABLE_SIZE; i++) // hashtable배열의 각 요소를 -1로 저장 
 		hashtable[i] = -1;
 
 	/*
@@ -339,31 +341,31 @@ int hashing(int *a, int **ht)
 		printf("hashtable[%d] = %d\n", i, hashtable[i]);
 	*/
 
-	int key = -1;
+	int key = -1; // 각 변수들 초기화 
 	int hashcode = -1;
 	int index = -1;
-	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++) 
 	{
-		key = a[i];
-		hashcode = hashCode(key);
+		key = a[i]; // key값에 a[i]저장 
+		hashcode = hashCode(key); // a[i]값을 베이스로 연산된 값 저장 
 		/*
 		printf("key = %d, hashcode = %d, hashtable[%d]=%d\n", key, hashcode, hashcode, hashtable[hashcode]);
 		*/
-		if (hashtable[hashcode] == -1)
+		if (hashtable[hashcode] == -1) // 연산된 위치의 배열의 값이 초기화되어있는 상태면 
 		{
-			hashtable[hashcode] = key;
+			hashtable[hashcode] = key; // key값으로 저장 
 		} else 	{
 
-			index = hashcode;
+			index = hashcode; // index에 hashcode저장 
 
-			while(hashtable[index] != -1)
+			while(hashtable[index] != -1) // hashtable[index]가 초기화되지 않았다면 
 			{
-				index = (++index) % MAX_HASH_TABLE_SIZE;
+				index = (++index) % MAX_HASH_TABLE_SIZE; // index % MAX_HASH_TABLE_SIZE로 연산을 하며 루프탈출까지 index ++  
 				/*
 				printf("index = %d\n", index);
 				*/
 			}
-			hashtable[index] = key;
+			hashtable[index] = key; // 루프를 탈출한 index위치의 hashtable배열에 key값 저장 
 		}
 	}
 
@@ -372,16 +374,16 @@ int hashing(int *a, int **ht)
 
 int search(int *ht, int key)
 {
-	int index = hashCode(key);
+	int index = hashCode(key); // key값을 기준으로 연산된 값을 index에 저장 
 
-	if(ht[index] == key)
-		return index;
+	if(ht[index] == key) // ht[index]의 키값이 인풋받은 key값과 같으면 
+		return index; // index를 리턴 
 
-	while(ht[++index] != key)
+	while(ht[++index] != key) // ht[index]가 key일때 까지 루프반복 후 index+1
 	{
-		index = index % MAX_HASH_TABLE_SIZE;
+		index = index % MAX_HASH_TABLE_SIZE; // 루프가 반복되는 동안 index연산  
 	}
-	return index;
+	return index; // index리턴 
 }
 
 
